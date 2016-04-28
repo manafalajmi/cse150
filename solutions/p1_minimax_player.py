da5c33390b58037c366bda40d5673fb8cb6a20fd
manafalajmi <manaf.alajmi@hotmail.com>
2016-04-28 13:16:59 -0700

    4th commit attempt, im getting tired now
    it works according to the tester, hopefully itll upload into the file

--- solutions/p1_minimax_player.py
+++ solutions/p1_minimax_player.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
-__author__ = 'Dan'
-__email__ = 'daz040@eng.ucsd.edu'
+__author__ = 'Manaf Alajmi Steven Zheng'
+__email__ = 'malajmi@ucsd.edu szzheng@ucsd.edu'
 
 from assignment2 import Player, State, Action
+
 
 class MinimaxPlayer(Player):
     def __init__(self):
@@ -15,4 +15,66 @@ class MinimaxPlayer(Player):
         :param state: State, the current state of the board.
         :return: Action, the next move
         """
-        raise NotImplementedError("Need to implement this method")
+        global currPly
+
+        #sets who the current player is, needed for rows
+        currPly = state.player
+        
+       
+
+        util = self.minmax(state, True)
+
+
+        return action
+
+
+
+    #minmax method, takes the current state of the board
+    #and a boolean that checks which recursive call this is
+    def minmax(self, state, num):
+         
+        #starts off by getting all possible actions
+        posActs=state.actions()
+    
+
+        global action
+        
+        
+        if (state.is_terminal()): 
+            return state.utility(currPly)
+        #checks if the game reached a terminal state
+        #and if it did it return the utility
+
+        #sets the util based on current player
+        if(currPly.row == state.player_row):
+            util =-1;
+        else:
+            util =1;
+            
+            #if there are no possible actions available
+        if not(posActs):
+            nextState = State(state.board, state.opponent_row, state.player)
+            util = self.minmax(nextState, False)
+        
+               
+        while(posActs):
+            currentAction =posActs.pop()
+            nextState = state.result(currentAction)
+ 
+            nextUtility = self.minmax(nextState, False)
+                
+            if (currPly.row == state.player_row):
+                if (util <= nextUtility):
+                    util = nextUtility;
+                    if (num):
+                        action = currentAction
+            else:
+                 if (util > nextUtility):
+                    util = nextUtility
+                    if (num):
+                        action = currentAction
+
+                
+                    
+
+        return util
